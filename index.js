28ad9028627542df76c8d0638dcb145625ce5e83
qiangjohnwang <qiangwangjohn@gmail.com>
2019-05-27 11:15:18 -0700

    NEW SITE!! woohoo.

--- index.js
+++ index.js
@@ -1,56 +1,65 @@
-//var prevScrollpos = window.pageYOffset;
-//
-//window.onscroll = function() {
-//    var currentScrollPos = window.pageYOffset;
-//    if (prevScrollpos > currentScrollPos) {
-//        document.getElementById("menuBar").style.top = "0";
-//    } else {
-//        document.getElementById("menuBar").style.top = "-100px";
-//    }
-//    prevScrollpos = currentScrollPos;
-//}
-//
-//var myIndex = document.getElementById('myIndex');
-//var myInfo = document.getElementById('myInfo');
-//
-//window.onclick = function(event) {
-//    if (event.target == myIndex) {
-//        myIndex.style.display = "none";
-//    } else if (event.target == myInfo) {
-//        myInfo.style.display = "none";
-//    }
-//}
-//
-//function openIndex() {
-//    document.getElementById("myIndex").style.display = "block";
-//}
-//
-//function closeIndex() {
-//    document.getElementById("myIndex").style.display = "none";
-//}
-//
-//function openInfo() {
-//    document.getElementById("myInfo").style.display = "block";
-//}
-//
-//function closeInfo() {
-//    document.getElementById("myInfo").style.display = "none";
-//}
-//
-//$(document).ready(function() {
-//    $(window).on("scroll resize", function() {
-//        var pos = $('.title').offset();
-//        $('.projectContainer').each(function() {
-//            if (pos.top >= $(this).offset().top && pos.top <= $(this).next().offset().top) {
-//                var projectName = $(this).find('.projectName');
-//
-//                $('.title').html(projectName.text());
-//                $('.title').prop('href', projectName.data('href'));
-//                return;
-//            }
-//        });
-//    });
-//    $(document).ready(function() {
-//        $(window).trigger('scroll');
-//    });
-//});
+$(document).ready(function () {
+          setTimeout(function () {
+               
+                $('a[href]#no-link').each(function () {
+                    var href = this.href;
+    
+                    $(this).removeAttr('href').css('cursor', 'pointer').click(function () {
+                        if (href.toLowerCase().indexOf("#") >= 0) {
+    
+                        } else {
+                            window.open(href, '_self');
+                        }
+                    });
+                });
+    
+          }, 500);
+    
+    setTimeout(function () {
+               
+                $('a[href]#no-link-blank').each(function () {
+                    var href = this.href;
+    
+                    $(this).removeAttr('href').css('cursor', 'pointer').click(function () {
+                        if (href.toLowerCase().indexOf("#") >= 0) {
+    
+                        } else {
+                            window.open(href, '_blank');
+                        }
+                    });
+                });
+    
+          }, 500);
+            
+          $('#indexButton').click (function() {
+            if ($('#indexContainer').css('opacity') == 0) {
+                $('#indexContainer').css({"opacity":"1", "visibility":"visible", "z-index":"1"});
+                $('#Inde').css({"opacity":"0", "visibility":"hidden"});
+                $('#x').css({"transform":"scale(2) translateX(-2pt)"});
+                $('#bioContainer').css({"opacity":"0", "visibility":"hidden"});
+                $('#who').css({"opacity":"1", "visibility":"visible"});
+                $('#whoX').css({"opacity":"0", "visibility":"hidden", "transform":"scale(1) translateX(0pt)"});
+            }
+            else{
+                $('#indexContainer').css({"opacity":"0", "visibility":"hidden", "z-index":"-1"}); 
+                $('#Inde').css({"opacity":"1", "visibility":"visible", "display":"inline-block"});
+                $('#x').css({"transform":"scale(1)"});
+            }
+          });
+            
+          $('#whoButton').click (function() {
+            if ($('#bioContainer').css('opacity') == 0) {
+                $('#bioContainer').css({"opacity":"1", "visibility":"visible", "z-index":"1"});
+                $('#who').css({"opacity":"0", "visibility":"hidden"});
+                $('#whoX').css({"opacity":"1", "visibility":"visible", "transform":"scale(2) translateX(-12pt)"});
+                $('#indexContainer').css({"opacity":"0", "visibility":"hidden"}); 
+                $('#Inde').css({"opacity":"1", "visibility":"visible", "display":"inline-block"});
+                $('#x').css({"transform":"scale(1)"});
+            }
+            else{
+                $('#bioContainer').css({"opacity":"0", "visibility":"hidden", "z-index":"-1"});
+                $('#who').css({"opacity":"1", "visibility":"visible"});
+                $('#whoX').css({"opacity":"0", "visibility":"hidden", "transform":"scale(1) translateX(0pt)"});
+            }
+          });
+});
