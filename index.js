270eec2a0ef0014bf4d0205be432e2045bcd2185
itsqiangwang <itsqiangwang@gmail.com>
2021-03-17 15:48:41 -0700

    styling + image hover in index

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-// let detailsTotal;
-// //total number of 'details' tags minus 1; excludes bio
-// detailsTotal = document.getElementsByTagName('details').length - 1; 
+let detailsTotal;
+//total number of 'details' tags minus 1; excludes bio
+detailsTotal = document.getElementsByTagName('details').length - 1; 
 
-// let detailsChosenNumber;
-// //randomly choose a number from [min + 1, max + 1] (i.e. [1,2] instead of [0,1]. exclude bio, which is 0)
-// detailsChosenNumber = Math.floor(Math.random() * detailsTotal) + 1;
+let detailsChosenNumber;
+//randomly choose a number from [min + 1, max + 1] (i.e. [1,2] instead of [0,1]. exclude bio, which is 0)
+detailsChosenNumber = Math.floor(Math.random() * detailsTotal) + 1;
 
-// let detailsSelected;
-// //open a selected 'details' tag
-// detailsSelected = document.getElementsByTagName('details')[detailsChosenNumber];
-// detailsSelected.setAttribute('open','true');
-// // detailsSelected.scrollIntoView({
-// //     block: 'start',
-// //     inline: 'start',
-// //     behavior: 'smooth'
-// // });
-
-// console.log('detailsTotal: ' + detailsTotal);
-// console.log('detailsChosenNumber: ' + detailsChosenNumber);
+let detailsSelected;
+//open a selected 'details' tag
+detailsSelected = document.getElementsByTagName('details')[detailsChosenNumber];
+detailsSelected.setAttribute('open','true');
+// detailsSelected.scrollIntoView({
+//     block: 'start',
+//     inline: 'start',
+//     behavior: 'smooth'
+// });
 
 let indexView = document.getElementById('index-view');
 let index = document.getElementById('index');
@@ -44,3 +41,6 @@ galleryView.onclick = function (){
     indexView.style.color = 'rgba(0,0,0,1)';
     indexView.style.textDecoration = 'none';
 }
+
+console.log('detailsTotal: ' + detailsTotal);
+console.log('detailsChosenNumber: ' + detailsChosenNumber);
