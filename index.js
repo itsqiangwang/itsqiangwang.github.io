79ebc64d13671a5e8c19809c6dc74c3c567bcce0
itsqiangwang <itsqiangwang@gmail.com>
2021-04-17 21:10:24 -0700

    gallery view + updated index

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-// let detailsTotal;
-// //total number of 'details' tags minus 1; excludes bio
-// detailsTotal = document.getElementsByTagName('details').length - 1; 
-
-// let detailsChosenNumber;
-// //randomly choose a number from [min + 1, max + 1] (i.e. [1,2] instead of [0,1]. exclude bio, which is 0)
-// detailsChosenNumber = Math.floor(Math.random() * detailsTotal) + 1;
-
-// let detailsSelected;
-// //open a selected 'details' tag
-// detailsSelected = document.getElementsByTagName('details')[detailsChosenNumber];
-// detailsSelected.setAttribute('open','true');
-// // detailsSelected.scrollIntoView({
-// //     block: 'start',
-// //     inline: 'start',
-// //     behavior: 'smooth'
-// // });
-
 let indexView = document.getElementById('index-view');
 let index = document.getElementById('index');
 let galleryView = document.getElementById('gallery-view');
 let gallery = document.getElementById('gallery');
+let navigation = document.getElementById('navigation');
 
-indexView.style.color = 'rgba(06,30,98,1)';
-indexView.style.textDecoration = 'underline';
+galleryView.style.color = 'rgba(06,30,98,1)';
+galleryView.style.textDecoration = 'underline';
 
 indexView.onclick = function (){
     gallery.style.display = 'none';
@@ -31,6 +14,7 @@ indexView.onclick = function (){
     indexView.style.textDecoration = 'underline';
     galleryView.style.color = 'rgba(0,0,0,1)';
     galleryView.style.textDecoration = 'none';
+    navigation.style.position='relative';
 }
 
 galleryView.onclick = function (){
@@ -40,6 +24,7 @@ galleryView.onclick = function (){
     galleryView.style.textDecoration = 'underline';
     indexView.style.color = 'rgba(0,0,0,1)';
     indexView.style.textDecoration = 'none';
+    navigation.style.position='sticky';
 }
 
 console.log('detailsTotal: ' + detailsTotal);
